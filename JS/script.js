e17814e12e6bbf0044ca5e9988a59e60734bd726
geekayush <srivastavs61@gmail.com>
2017-06-24 15:44:26 +0530

    First commit/ Add files

---
+++ JS/script.js
@@ -0,0 +1,35 @@
+//autocomplete seach bar
+
+$("#search_bar").autocomplete({
+    source: function (request, response) {
+        $.ajax({
+            url: "http://en.wikipedia.org/w/api.php",
+            dataType: "jsonp",
+            data: {
+                'action': "opensearch",
+                'format': "json",
+                'search': request.term
+            },
+            success: function (data) {
+                $('#links').empty();
+                link_generator(data);
+            }
+        });
+    }
+});
+
+//link generator
+
+function link_generator(data) {
+    var i = 0;
+    title = data[1];
+    description = data[2];
+    link = data[3];
+
+    for (i; i < data[3].length; i++) {
+        var link_list = '<a href="' + link[i] + '">' + title[i] + '</a>';
+        var description_list = '<li>' + link_list + '<br><p>' + description[i] + '</p><hr></li>';
+
+        $('#links').append(description_list);
+    }
+}
